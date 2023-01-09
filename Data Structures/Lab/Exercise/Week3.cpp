ad2085841287c3c1776ba1d6f6c45fba668a8567
satmika ozora <mikaozora08@gmail.com>
2023-01-09 16:18:40 +0700

    update

--- Data Structures/Lab/Exercise/Week3.cpp
+++ Data Structures/Lab/Exercise/Week3.cpp
@@ -14,43 +14,60 @@ struct Food *createFood(int price)
     food->next = NULL;
 }
 
-void pushHead(int price){
+void pushHead(int price)
+{
     struct Food *newFood = createFood(price);
-    if(!head){
+    if (!head)
+    {
         head = tail = newFood;
-    }else{
+    }
+    else
+    {
         newFood->next = head;
         head = newFood;
     }
 }
 
-void printFoods(){
+void printFoods()
+{
     struct Food *curr = head;
-    while(curr){
+    while (curr)
+    {
         printf("%d\n", curr->price);
         curr = curr->next;
     }
 }
 
-void pushTail(int price){
+void pushTail(int price)
+{
     struct Food *newFood = createFood(price);
-    if(!head){
+    if (!head)
+    {
         head = tail = newFood;
-    }else{
+    }
+    else
+    {
         tail->next = newFood;
         tail = newFood;
     }
 }
 
-void pushMid(int price){
-    if(!head || price < head->price){
+void pushMid(int price)
+{
+    if (!head || price < head->price)
+    {
         pushHead(price);
-    }else if(price >= tail->price){
+    }
+    else if (price >= tail->price)
+    {
         pushTail(price);
-    }else{
+    }
+    else
+    {
         struct Food *newFood = createFood(price);
         struct Food *curr = head;
-        while(curr->next->price < price && curr->next != NULL){
+        while (curr->next->price < price && curr->next != NULL)
+        {
             curr = curr->next;
         }
         newFood->next = curr->next;
@@ -58,28 +75,41 @@ void pushMid(int price){
     }
 }
 
-void popHead(){
-    if(!head){
+void popHead()
+{
+    if (!head)
+    {
         return;
-    }else if(head == tail){
+    }
+    else if (head == tail)
+    {
         free(head);
         head = tail = NULL;
-    }else{
+    }
+    else
+    {
         struct Food *temp = head->next;
         free(head);
         head = temp;
     }
 }
 
-void popTail(){
-    if(!head){
+void popTail()
+{
+    if (!head)
+    {
         return;
-    }else if(head == tail){
+    }
+    else if (head == tail)
+    {
         free(head);
         head = tail = NULL;
-    }else{
+    }
+    else
+    {
         struct Food *temp = head;
-        while(temp->next != tail){
+        while (temp->next != tail)
+        {
             temp = temp->next;
         }
         free(tail);
@@ -88,9 +118,44 @@ void popTail(){
     }
 }
 
+void popMid(int price)
+{
+    if (!head)
+    {
+        return;
+    }
+    else if (price == head->price)
+    {
+        popHead();
+    }
+    else if (price == tail->price)
+    {
+        popTail();
+    }
+    else
+    {
+        struct Food *curr = head;
+        while (price != curr->next->price)
+        {
+            curr = curr->next;
+            if (curr->next == NULL)
+            {
+                break;
+            }
+        }
+        if (curr->next)
+        {
+            struct Food *temp = curr->next->next;
+            free(curr->next);
+            curr->next = temp;
+        }else{
+            printf("Data not found\n");
+        }
+    }
+}
+
 int main()
 {
-    int arr[100] = {1, 2, 3};
 
     // struct Food *food = createFood(20000);
     pushMid(1000);
@@ -98,8 +163,9 @@ int main()
     pushMid(3000);
     pushMid(2500);
     pushMid(2501);
-    popHead();
-    popTail();
+    // popHead();
+    // popTail();
+    popMid(2501);
     // printf("%d\n", head->next->price);
     printFoods();
 }
